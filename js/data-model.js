298e04f5b1c6e214658e50e5ca5fb1cbce486447
Marc <marcstruchmalonda@gmail.com>
2025-11-19 23:46:55 +0100

    Estructura htm realizada y clase tarea terminada y agenda terminada

--- js/data-model.js
+++ js/data-model.js
@@ -1,6 +1,28 @@
-class Tarea{
+//clase Tarea
+class Tarea {
     //constructor
     constructor(titulo){
+        this.id = Date.now();
+        this.titulo = titulo;
+        this.completado = false;
+        const ID = new Date();
+    }
 
+    //Metodo para completar tarea
+    marcarCompletada(){
+        this.completado = true;
+    }
+}
+
+//Clase Agenda para gestionar tareas Array y POO
+class Agenda {
+    #tareas = []; //Array privado para almacenar objetos
+
+    //Metodo para agregar tarea
+    agregar(titulo){
+        const nuevaTarea = new Tarea(titulo)
+        this.#tareas.push(nuevaTarea);
+        Agenda.TAREAS_TOTALES++;
+        return nuevaTarea;
     }
 }
